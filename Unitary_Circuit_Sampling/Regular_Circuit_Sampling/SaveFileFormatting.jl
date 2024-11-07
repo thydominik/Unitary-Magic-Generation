a15090df4b97592ac35fc9d9ee2090817a2d85d9
Dominik Szombathy <84455817+thydominik@users.noreply.github.com>
2024-11-07 14:48:01 +0100

    Update SaveFileFormatting.jl

--- Unitary_Circuit_Sampling/Regular_Circuit_Sampling/SaveFileFormatting.jl
+++ Unitary_Circuit_Sampling/Regular_Circuit_Sampling/SaveFileFormatting.jl
@@ -1,3 +1,11 @@
 using JLD2
 using MAT
 
+for N in 2:5
+    fname1 = "RegularUnitaryCircuitMagicSampled_N_$(N)_Samples_1048576_Seed_1.jdl2"
+    data = JLD2.load(fname1)
+    fname2 = "RegularUnitaryCircuitMagicSampled_N_$(N)_Samples_1048576_Seed_1.mat"
+    matwrite(fname2, data)
+end
+
+data = JLD2.load("RegularUnitaryCircuitMagicSampled_N_2_Samples_1048576_Seed_1.jdl2")
