d0f9b42d86f9d90ceded6dd062b11f7762878056
Dominik Szombathy <84455817+thydominik@users.noreply.github.com>
2026-01-19 17:24:30 +0100

    Create Utils.jl skeleton for utility functions

---
+++ src/utils/Utils.jl
@@ -0,0 +1,10 @@
+"""
+    Utils module
+
+Utility functions for UnitaryMagicGeneration package.
+Includes helper functions for numerical integration and other utilities.
+"""
+
+# Include utility submodules when they are refactored
+include("numerical_integration.jl")
+# include("helpers.jl")
