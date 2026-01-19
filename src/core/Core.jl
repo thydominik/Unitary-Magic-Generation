df4c37642e11ab37cdda83b68c7bc7d7bfa1bdff
Dominik Szombathy <84455817+thydominik@users.noreply.github.com>
2026-01-19 17:24:24 +0100

    Create Core.jl skeleton for core functionality

---
+++ src/core/Core.jl
@@ -0,0 +1,11 @@
+"""
+    Core module
+
+Core functionality for UnitaryMagicGeneration package.
+This module contains the main algorithmic components.
+"""
+
+# Include core submodules when they are refactored
+# include("pauli_operations.jl")
+# include("magic_measurements.jl")
+# include("unitary_generation.jl")
