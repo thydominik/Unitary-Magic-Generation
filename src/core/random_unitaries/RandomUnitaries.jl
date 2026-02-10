789437c1d886dfffc33839c0bb64dabc6b7d79a9
Dominik Szombathy <szombathy.dominik@gmail.com>
2026-02-10 20:13:23 +0100

    Delete RandomUnitaries.jl

--- src/core/random_unitaries/RandomUnitaries.jl
+++
@@ -1,8 +0,0 @@
-# Deprecated compatibility file.
-#
-# Canonical implementation lives in:
-#   src/core/random_unitaries/random_unitaries.jl
-#
-# This file keeps existing include paths working while the repository is refactored.
-
-include("random_unitaries.jl")
