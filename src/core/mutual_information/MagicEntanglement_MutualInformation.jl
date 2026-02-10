cf43b6571026574807b840750cc54a8a3ce1f1e5
Dominik Szombathy <szombathy.dominik@gmail.com>
2026-02-10 20:34:13 +0100

    Deletetion

--- src/core/mutual_information/MagicEntanglement_MutualInformation.jl
+++
@@ -1,7 +0,0 @@
-# Deprecated compatibility file.
-#
-# Analysis scripts were moved out of src/core.
-# Canonical implementation lives in:
-#   src/core/mutual_information/mutual_information.jl
-
-include("mutual_information.jl")
--- src/core/utilities/Utils.jl
+++
@@ -1,8 +0,0 @@
-# Deprecated compatibility file.
-#
-# Canonical implementation lives in:
-#   src/core/utilities/utilities.jl
-#
-# This file keeps existing include paths working while the repository is refactored.
-
-include("utilities.jl")
