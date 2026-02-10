ff979d0de7784b2a67335eae502d2b9c3b903ff8
Dominik Szombathy <szombathy.dominik@gmail.com>
2026-02-10 19:48:46 +0100

    src/core/magic done

--- src/core/magic/Magic_2.jl
+++
@@ -1,6 +0,0 @@
-# Deprecated compatibility wrapper.
-#
-# The implementation has been consolidated into `magic.jl` and the `magic` module.
-# This file remains only to avoid breaking legacy includes.
-
-include("magic.jl")
--- src/core/magic/MaxMagic.jl
+++
@@ -1,4 +0,0 @@
-# This script has been moved out of core code.
-#
-# See: research/max_magic.jl
-error("src/core/magic/MaxMagic.jl has moved to research/max_magic.jl")
